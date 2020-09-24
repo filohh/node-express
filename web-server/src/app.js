609e4fe316fd22054004ec8dec92a893b68c0b75
Anfilofio Coutinho <filo6720@gmail.com>
2020-09-24 08:59:50 -0300

    V2409U02

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -55,7 +55,7 @@ app.get('/weather', (req, res) => {
     if (error) {
       return res.send(error)
     }
-    console.log('geocode', location)
+    
     forecast(address, longitude, (error, forecastData) => {
       if (forecastData === undefined) {
         return res.send({
@@ -65,7 +65,7 @@ app.get('/weather', (req, res) => {
       }
       
       return res.send({
-        Location: address,
+        Location: location,
         Forecast: forecastData,
         })
       })
